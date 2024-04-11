fb476662e5efa748b6a09b94693be783d5eda28e
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-04-11 07:42:45 -0300

    Update ponderada.c

--- ponderada.c
+++ ponderada.c
@@ -6,41 +6,37 @@
 #define NOTAS 3
 
 int main(){
-	setlocale(LC_ALL, "");
-	//declarando variaveis.
-	char disciplina[DISCIPLINA][200];
-	float somaPeso = 0, resultado[DISCIPLINA], somaMedia = 0, notas[DISCIPLINA][NOTAS], peso[NOTAS];
-	int i, j;
+setlocale(LC_ALL, "");
+//declarando variaveis.
+char disciplina[DISCIPLINA][200];
+float somaPeso = 0, resultado[DISCIPLINA], somaMedia = 0, notas[DISCIPLINA][NOTAS], peso[NOTAS];
+int i, j;
 	
-	//solicitando dados do usu·rio.
+//solicitando dados do usu√°rio.
 	for(i=0;i<DISCIPLINA;i++){
-		printf("\nDigite %i™ disciplina:", i+1);
+		printf("\nDigite %i¬™ disciplina:", i+1);
 		scanf("%s",&disciplina[i]);
-			for(j=0;j<NOTAS;j++){
-				printf("\nDigite %i™ nota:", j+1);
-				scanf("%f",&notas[i][j]);
-				printf("\nDigite o peso da %i™ nota:", j+1);
-				scanf("%f",&peso[j]);
+		for(j=0;j<NOTAS;j++){
+		printf("\nDigite %i¬™ nota:", j+1);
+		scanf("%f",&notas[i][j]);
+		printf("\nDigite o peso da %i¬™ nota:", j+1);
+		scanf("%f",&peso[j]);
 			
-		 	somaMedia += (notas[i][j] * peso[j]);
-            somaPeso += peso[j];
-			}		
-			resultado[i] = somaMedia / somaPeso;
-			somaPeso=0;
-			somaMedia=0;
+		somaMedia += (notas[i][j] * peso[j]);
+           	somaPeso += peso[j];
+		}		
+		resultado[i] = somaMedia / somaPeso;
+		somaPeso=0;
+		somaMedia=0;
 	}
-	//exibindo resultados
-		for(i=0;i<DISCIPLINA;i++){
-		printf("\nDisciplina: %s", disciplina[i]);
-		
-			for(j=0;j<NOTAS;j++){
-				printf("\n%i™ nota: %.1f", j+1, notas[i][j]);
-				
-			}
-	
-		printf("\nMÈdia ponderada: %.1f", resultado[i]);	
+//exibindo resultados
+	for(i=0;i<DISCIPLINA;i++){
+	printf("\nDisciplina: %s", disciplina[i]);
+		for(j=0;j<NOTAS;j++){
+		printf("\n%i¬™ nota: %.1f", j+1, notas[i][j]);		
+		}
+	printf("\nM√©dia ponderada: %.1f", resultado[i]);	
 	}
-	
 	return 0;
 //fim do programa	
 }
